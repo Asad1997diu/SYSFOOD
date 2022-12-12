b949bcf20be3411c979a3779876b6afd28daac35
MSI <asad1996diu@gmail.com>
2022-12-13 00:55:24 +0600

    First Commit

--- lib/modules/splash/splesh_screens.dart
+++ lib/modules/splash/splesh_screens.dart
@@ -5,40 +5,41 @@ import 'package:sysfood/modules/splash/splash_controller.dart';
 
 class SplashScreen extends GetView<SplashController> {
   const SplashScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFFff0036),
-              Color(0xFFff6787),
-            ]
-        )
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: const [
-            AnimatedPositioned(
-              duration: Duration(microseconds: 1600),
+      return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFFff0036),
+                  Color(0xFFff6787),
+                ]
+            )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: const [
+              AnimatedPositioned(
+                duration: Duration(microseconds: 1600),
                 top: 300,
                 left: 100,
                 child: Center(
                   child: Image(image: AssetImage(Assets.imagesSysfoodlogo),),
                 ),
-            ),
-            Positioned(
+              ),
+              Positioned(
                 bottom: 0,
                 right: 0,
                 child: Image(image: AssetImage(Assets.imagesBurger),),
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
 
